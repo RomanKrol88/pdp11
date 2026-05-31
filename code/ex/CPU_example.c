23b674003b95ceb52d85bf7e122f07b25c382ad5
Chainik_mgn <chainik7@list.ru>
2026-05-31 20:52:24 +0500

    исправлен тип данных на Data (unsigned char), добавлены комментарии

--- code/ex/CPU_example.c
+++ code/ex/CPU_example.c
@@ -2,15 +2,56 @@
 
 #define N 4 //количество ячеек памяти
 
-int scan_code_adress();                                                                         //функция считывания адреса
-int code_adress_to_reg();                                                                       //функция расшифровки кода адреса
-int func_ADD(int adress_reg1, int adress_reg2, int * reg_array);         //функция ADD сложения чисел
-int func_SUB(int first_arg, int second_arg, int * reg_array);         //функция SUB вычитания чисел
-void func_MOV(int arg, int value, int * reg_array);                 //функция MOV запись числа в регистр
-void print_IR(int * reg_array);                                          //функция печати всех чисел в памяти
+typedef unsigned char Data;
 
-int code_adress_to_reg(int adress_reg) {
-    int reg_number;
+Data scan_code_adress();                                                    //функция считывания адреса
+Data code_adress_to_reg(Data adress_reg);                                   //функция расшифровки кода адреса
+Data func_ADD(Data adress_reg1, Data adress_reg2, Data * reg_array);        //функция ADD сложения чисел
+Data func_SUB(Data first_arg, Data second_arg, Data * reg_array);           //функция SUB вычитания чисел
+void func_MOV(Data arg, Data value, Data * reg_array);                      //функция MOV запись числа в регистр
+void print_IR(Data * reg_array);                                            //функция печати всех чисел в памяти
+
+int main() {
+    Data function_code;              //переменная хранит код вызова функции
+    Data value;                      //переменная хранит значение, которое хотим записать в память
+    Data first_code_adress, second_code_adress, code_adress; //переменные для временного хранения полученного кода адреса регистра 
+    Data reg_array[N] = {0};         //массив для записи и хранения значений в регистрах памяти
+
+    while (scanf("%hhu", &function_code) == 1) {
+            switch (function_code) {
+            case 1 : 
+                first_code_adress = scan_code_adress();
+                second_code_adress = scan_code_adress();
+                func_ADD(first_code_adress, second_code_adress, reg_array);
+                break;
+            case 2 : 
+                first_code_adress = scan_code_adress();
+                second_code_adress = scan_code_adress();
+                func_SUB(first_code_adress, second_code_adress, reg_array);
+                break;
+            case 3 : 
+                code_adress = scan_code_adress();
+                scanf("%hhu", &value);
+                func_MOV(code_adress, value, reg_array);
+                break;
+            case 4 :
+                print_IR(reg_array);
+                break;
+            case 0 :
+            default : return 0;
+        }
+    }
+    return 0;
+}
+
+Data scan_code_adress() {
+    Data adress_reg;
+    scanf("%hhu", &adress_reg);
+    return adress_reg;
+}
+
+Data code_adress_to_reg(Data adress_reg) {
+    Data reg_number;
 
     switch (adress_reg) {
     case 5 :
@@ -26,66 +67,25 @@ int code_adress_to_reg(int adress_reg) {
         reg_number = 3;
         break;
     }
-    
+
     return reg_number;
 }
 
-int scan_code_adress() {
-    int adress_reg;
-    scanf("%d ", &adress_reg);
-    return adress_reg;
-}
-
-int main() {
-    int function_code;              //переменная хранит код вызова функции
-    int value;
-    int first_code_adress, second_code_adress, code_adress;
-    int reg_array[N] = {0};
-
-    while (scanf("%d", &function_code) == 1) {
-    
-        switch (function_code) {
-            case 1 : 
-                first_code_adress = scan_code_adress();
-                second_code_adress = scan_code_adress();
-                func_ADD(first_code_adress, second_code_adress, reg_array);
-                break;
-            case 2 : 
-                first_code_adress = scan_code_adress();
-                second_code_adress = scan_code_adress();
-                func_SUB(first_code_adress, second_code_adress, reg_array);
-                break;
-            case 3 : 
-                code_adress = scan_code_adress();
-                scanf("%d", &value);
-                func_MOV(code_adress, value, reg_array);
-                break;
-            case 4 :
-                print_IR(reg_array);
-                break;
-            case 0 :
-            default : return 0;
-        }
-    }
-    return 0;
-}
-
-int func_ADD(int first_arg, int second_arg, int * reg_array) {
+Data func_ADD(Data first_arg, Data second_arg, Data * reg_array) {
     return reg_array[code_adress_to_reg(first_arg)] +=  reg_array[code_adress_to_reg(second_arg)];
 }
 
-int func_SUB(int first_arg, int second_arg, int * reg_array) {
+Data func_SUB(Data first_arg, Data second_arg, Data * reg_array) {
     return reg_array[code_adress_to_reg(first_arg)] -=  reg_array[code_adress_to_reg(second_arg)];
 }
 
-
-void func_MOV(int arg, int value, int * reg_array) {
+void func_MOV(Data arg, Data value, Data * reg_array) {
     reg_array[code_adress_to_reg(arg)] = value;
 }
 
-void print_IR(int * reg_array) {
-    for (int i = 0; i < N; i++) {
-        printf("%d ", reg_array[i]);
+void print_IR(Data * reg_array) {
+    for (Data i = 0; i < N; i++) {
+        printf("%hhu ", reg_array[i]);
     }
     printf("\n");
 }
